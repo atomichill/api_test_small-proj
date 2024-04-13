254f7a79d7e9b6720e77308e50292f7890c97813
giorgi khabazishvili <khabazishvili20@gmail.com>
2024-04-13 17:28:35 +0400

    finishing dog api tests

--- test_dog_api.py
+++ test_dog_api.py
@@ -23,7 +23,29 @@ def test_get_rnd_img(base_url):
 
 
 @pytest.mark.parametrize("breed", ["hound", "beagle", "bouvier", "borzoi"])
-def test_get_rnd_img_breed(base_url):
-    r = requests.get(base_url + "/breed/" + "breed" + "/images")
+def test_get_imgs_breed(base_url,breed):
+
+    r = requests.get(base_url + "/breed/" + breed + "/images")
 
     assert r.status_code == 200
+
+
+@pytest.mark.parametrize("breed", ["hound", "beagle", "bouvier", "borzoi"])
+def test_get_rnd_img_breed(base_url, breed):
+    r = requests.get(base_url + "/breed/" + breed + "/images" + "/random")
+    v = cerberus.Validator()
+
+    scheme = {
+            "message": {"type": "string"},
+            "status": {"type":"string"}
+    }
+
+    assert r.status_code == 200
+    assert v.validate(r.json(),scheme)
+
+@pytest.mark.parametrize("breed", ["hound", "beagle", "bouvier", "borzoi"])
+def test_get_subbreed(base_url, breed):
+    r = requests.get(base_url + "/breed/" + breed + "/list")
+
+    assert r.status_code == 200
+
--- test_get.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import requests
-
-
-def test_get(base_url):
-    r = requests.get(base_url)
-    assert r.status_code == 200
